eb2918fa8ce4d5937bb9a0ccd7228481f8209982
Jonah <jonahboan@gmail.com>
2019-05-28 20:10:35 -0400

    Added intake stuff and added cubic function

--- BonusLevel/Robot.cs
+++ BonusLevel/Robot.cs
@@ -17,20 +17,28 @@ namespace BonusLevel {
         CTRE.Phoenix.PneumaticControlModule pcm = new PneumaticControlModule(0);
 
         TalonSRX leftMaster = new TalonSRX(RobotMap.LEFT_MASTER);
-        //TalonSRX leftSlave1 = new TalonSRX(RobotMap.LEFT_SLAVE1);
-        //TalonSRX leftSlave2 = new TalonSRX(RobotMap.LEFT_SLAVE2);
-        //TalonSRX rightMaster = new TalonSRX(RobotMap.RIGHT_MASTER);
-        //TalonSRX rightSlave1 = new TalonSRX(RobotMap.RIGHT_SLAVE1);
-        //TalonSRX rightSlave2 = new TalonSRX(RobotMap.RIGHT_SLAVE2);
+        TalonSRX leftSlave1 = new TalonSRX(RobotMap.LEFT_SLAVE1);
+        TalonSRX leftSlave2 = new TalonSRX(RobotMap.LEFT_SLAVE2);
+        TalonSRX rightMaster = new TalonSRX(RobotMap.RIGHT_MASTER);
+        TalonSRX rightSlave1 = new TalonSRX(RobotMap.RIGHT_SLAVE1);
+        TalonSRX rightSlave2 = new TalonSRX(RobotMap.RIGHT_SLAVE2);
+        TalonSRX intake1 = new TalonSRX(RobotMap.INTAKE1);
+        TalonSRX intake2 = new TalonSRX(RobotMap.INTAKE2);
 
         private State currentState = State.None, newState = State.Disabled;
 
         private void robotInit() {
             js0 = new GameController(UsbHostDevice.GetInstance());
-            //leftSlave1.Follow(leftMaster);
-            //leftSlave2.Follow(leftMaster);
-            //rightSlave1.Follow(rightMaster);
-            //rightSlave2.Follow(rightMaster);
+            leftSlave1.Follow(leftMaster);
+            leftSlave2.Follow(leftMaster);
+            rightSlave1.Follow(rightMaster);
+            rightSlave2.Follow(rightMaster);
+
+            intake2.Follow(intake1);
+
+            rightMaster.SetInverted(true);
+            rightSlave1.SetInverted(true);
+            rightSlave2.SetInverted(true);
         }
 
         private void robotPeriodic() {
@@ -49,32 +57,54 @@ namespace BonusLevel {
             if (haveEnabledButtonsBeenReleased && (js0.GetButton(RobotMap.BACK) || js0.GetButton(RobotMap.START)) && currentState == State.Enabled) {
                 switchState(State.Disabled);
             }
-
-            pcm.StartCompressor();
         }
 
         private void enabledInit() {
             Debug.Print("Enabled");
+            pcm.StartCompressor();
+
             leftMaster.SetNeutralMode(NeutralMode.Coast);
-            //rightMaster.SetNeutralMode(NeutralMode.Coast);
+            leftSlave1.SetNeutralMode(NeutralMode.Coast);
+            leftSlave2.SetNeutralMode(NeutralMode.Coast);
+            rightMaster.SetNeutralMode(NeutralMode.Coast);
+            rightSlave1.SetNeutralMode(NeutralMode.Coast);
+            rightSlave2.SetNeutralMode(NeutralMode.Coast);
         }
 
         private void enabledPeriodic() {
             // 2 - left and right, right stick
             // 5 - up and down, right stick
-            float power = -1 * js0.GetAxis(5);
-            double rotate = 0.4 * js0.GetAxis(2);
-            drivePercentage(power, (float)rotate);
+            float power = -1 * js0.GetAxis(RobotMap.DRIVE_AXIS);
+
+            // cubic function generated with ti regression model
+            // 4x^3 - 6x^2 + 3x
+            power = (float)(4 * Math.Pow(power, 3) - 6 * Math.Pow(power, 2) + 3 * power);
+
+            float rotate = -0.4f * js0.GetAxis(RobotMap.ROTATE_AXIS);
+            drivePercentage(power, rotate);
+
+            // Intake
+            if (js0.GetButton(RobotMap.INTAKE_IN) && !js0.GetButton(RobotMap.INTAKE_OUT)) {
+                intake1.Set(ControlMode.PercentOutput, 0.60);
+            } else if (!js0.GetButton(RobotMap.INTAKE_IN) && js0.GetButton(RobotMap.INTAKE_OUT)) {
+                intake1.Set(ControlMode.PercentOutput, -1);
+            } else {
+                intake1.Set(ControlMode.PercentOutput, 0);
+            }
         }
 
         private void disabledInit() {
             Debug.Print("Disabled");
             leftMaster.SetNeutralMode(NeutralMode.Brake);
-            //rightMaster.SetNeutralMode(NeutralMode.Brake);
+            leftSlave1.SetNeutralMode(NeutralMode.Brake);
+            leftSlave2.SetNeutralMode(NeutralMode.Brake);
+            rightMaster.SetNeutralMode(NeutralMode.Brake);
+            rightSlave1.SetNeutralMode(NeutralMode.Brake);
+            rightSlave2.SetNeutralMode(NeutralMode.Brake);
+            pcm.StopCompressor();
         }
 
         private void disabledPeriodic() {
-            drivePercentage(0, 0);
         }
 
         double[] arcadeDrive(float xSpeed, float zRotation) {
@@ -112,6 +142,17 @@ namespace BonusLevel {
             return new double[] { leftMotorOutput, rightMotorOutput };
         }
 
+        public void drivePercentage(float speed, float rotation) {
+            double[] speeds = arcadeDrive(speed, rotation);
+            leftMaster.Set(ControlMode.PercentOutput, speeds[1]);
+            rightMaster.Set(ControlMode.PercentOutput, speeds[0]);
+        }
+
+
+
+        /// ////////////////////////////////
+        /// DO NOT TOUCH!!!
+        //////////////////////////////////////
         public void feed() {
             // Run robotInit if there is no current state
             if (currentState == State.None) {
@@ -168,13 +209,6 @@ namespace BonusLevel {
 
             lastConnectionStatus = currentConnectionStatus;
         }
-
-        public void drivePercentage(float speed, float rotation) {
-            double[] speeds = arcadeDrive(speed, rotation);
-            leftMaster.Set(ControlMode.PercentOutput, speeds[1]);
-            //rightMaster.Set(ControlMode.PercentOutput, speeds[0]);
-        }
-
 
 
 
--- BonusLevel/RobotMap.cs
+++ BonusLevel/RobotMap.cs
@@ -3,14 +3,22 @@ using Microsoft.SPOT;
 
 namespace BonusLevel {
     public class RobotMap {
-        // Controller buttons
+        // Button IDs
         public const int BACK = 9;
         public const int START = 10;
-        public const int RIGHT_MASTER = 4;
-        public const int RIGHT_SLAVE1 = 2;
-        public const int RIGHT_SLAVE2 = 3;
+        public const int INTAKE_IN = 6;
+        public const int INTAKE_OUT = 5;
+        public const int DRIVE_AXIS = 1;
+        public const int ROTATE_AXIS = 2;
+
+        // Motor Controller IDs
         public const int LEFT_MASTER = 1;
-        public const int LEFT_SLAVE1 = 5;
-        public const int LEFT_SLAVE2 = 6;
+        public const int LEFT_SLAVE1 = 2;
+        public const int LEFT_SLAVE2 = 3;
+        public const int RIGHT_MASTER = 4;
+        public const int RIGHT_SLAVE1 = 5;
+        public const int RIGHT_SLAVE2 = 6;
+        public const int INTAKE1 = 7;
+        public const int INTAKE2 = 8;
     }
 }
